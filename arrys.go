b0950140cf5cbef22a8c38f99a45cec0395f2849
harvey <hxharveydev@gmail.com>
2022-03-27 14:51:59 +0700

    add array

--- arrys.go
+++ arrys.go
@@ -2,8 +2,43 @@ package main
 
 import "fmt"
 
+//值传递，拷贝数组
+func printArray(arr [5]int) {
+	for i, v := range arr {
+		fmt.Println(i, v) //索引和值
+	}
+}
+
+//指针
+func printPArray(arr *[5]int) {
+	for i, v := range arr {
+		fmt.Println(i, v) //索引和值
+	}
+}
 func main() {
+
 	var arr1 [5]int
 
-	fmt.Println(arr1)
+	arr2 := [3]int{1, 3, 5}
+
+	arr3 := [...]int{2, 4, 6, 8, 10}
+
+	var grid [4][5]int
+
+	fmt.Println(arr1, arr2, arr3, grid)
+
+	for i := 0; i < len(arr3); i++ {
+		fmt.Println(arr3[i])
+	}
+
+	for i := range arr3 {
+		fmt.Println(arr3[i]) //同上
+	}
+
+	for i, v := range arr3 {
+		fmt.Println(i, v) //索引和值
+	}
+	printArray(arr1)
+	//printArray(arr2)
+	printArray(arr3)
 }
